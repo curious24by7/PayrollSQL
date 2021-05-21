0e917eed49d2030b1642de4bae6ca3d1f46d54c5
sohamdhamnaskar <sohamdham123@gmail.com>
2021-05-21 22:54:48 +0530

    [soham]Added: Table

--- Payroll.sql
+++ Payroll.sql
@@ -1,1 +1,15 @@
 create database payroll_service
+
+select * from sys.databases;
+
+use payroll_service
+
+select DB_NAME();
+
+create table employee_payroll
+(
+ id int identity(1,1) primary key,
+ name varchar(100) not null,
+ salary money not null,
+ start date not null
+);
